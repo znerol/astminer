160faff5164f89c9adb8f78abbf898067576ba56
Lorenz Schori <lo@znerol.ch>
2011-02-19 16:10:07 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='astminer',
+      version='0.1',
+      description='Asterisk integration for the Redmine issue tracker.',
+      author='Lorenz Schori',
+      author_email='lo@znerol.ch',
+      url='http://github.com/znerol/astminer',
+      scripts=['astminer.py']
+)
+
