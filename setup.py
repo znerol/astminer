effdc6004e2cbbb97588066febcfce69379f127a
Lorenz Schori <lo@znerol.ch>
2011-02-20 15:14:26 +0100

    Update setup.py, add MANIFEST.in

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@
 from distutils.core import setup
 
 setup(name='astminer',
-      version='0.1',
-      description='Asterisk integration for the Redmine issue tracker.',
-      author='Lorenz Schori',
-      author_email='lo@znerol.ch',
-      url='http://github.com/znerol/astminer',
-      scripts=['astminer.py']
+    version='0.1',
+    description='Asterisk integration for the Redmine issue tracker.',
+    author='Lorenz Schori',
+    author_email='lo@znerol.ch',
+    url='http://github.com/znerol/astminer',
+    scripts=['astminer.py'],
+    data_files=[
+        ('share/doc/astminer/examples', ['astminer.conf']),
+        ('share/doc/astminer', ['README', 'LICENSE']),
+    ],
 )
 
